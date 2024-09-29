c621480a160713d820abf2853ac4cd3bf7ccb4b9
haseebasif <haseebasif215@gmail.com>
2024-09-29 16:58:11 +0500

    Buttons color changed

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -154,7 +154,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
       <button className="relative inline-flex overflow-hidden rounded-full p-[1px] ">
         <span
           className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite]
-         bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
+         bg-[conic-gradient(from_90deg_at_50%_50%,#00D1FF_0%,#D49B66_50%,#00D1FF_100%)]"
         />
         <span
           className="inline-flex h-full w-full cursor-pointer items-center 
